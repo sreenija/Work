45a1906a621a058cea9033b0e1295e38b4a5aaf5
Surya Kiran <schittib@mtu.edu>
2017-04-29 20:47:04 -0400

    updating print msg in hello.pl

--- perl/hello.pl
+++ perl/hello.pl
@@ -4,6 +4,7 @@ my $b = 3;
 my $c = 10;
 {
 open my $fh, '>', 'output.txt';
+print {$fh} "Printing values from variables in program to this file";
 print {$fh} $a . "\n";
 print {$fh} $b . "\n";
 print {$fh} $c . "\n";
