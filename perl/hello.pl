0300fc57683e834c97afc90f3cc6f05abd541176
sreenija <speram@mtu.edu>
2017-04-29 20:39:49 -0400

    frst program

---
+++ perl/hello.pl
@@ -0,0 +1,19 @@
+#!usr/bin/perl
+my $a = 5;
+my $b = 3;
+my $c = 10;
+{
+open my $fh, '>', 'output.txt';
+print {$fh} $a . "\n";
+print {$fh} $b . "\n";
+print {$fh} $c . "\n";
+}
+
+{
+open my $fh, '<', 'output.txt';
+my ($a, $b, $c) = <$fh>;
+print $a;
+print $b;
+print $c;
+close $fh;
+}
