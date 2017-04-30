773a461a3750998651bd425f4ac7428f187d982b
sreenija <speram@mtu.edu>
2017-04-29 20:47:25 -0400

    fourth element added

--- perl/hello.pl
+++ perl/hello.pl
@@ -2,18 +2,21 @@
 my $a = 5;
 my $b = 3;
 my $c = 10;
+my $d = 7;
 {
 open my $fh, '>', 'output.txt';
 print {$fh} $a . "\n";
 print {$fh} $b . "\n";
 print {$fh} $c . "\n";
+print {$fh} $d . "\n";
 }
 
 {
 open my $fh, '<', 'output.txt';
-my ($a, $b, $c) = <$fh>;
+my ($a, $b, $c, $d) = <$fh>;
 print $a;
 print $b;
 print $c;
+print $d;
 close $fh;
 }
